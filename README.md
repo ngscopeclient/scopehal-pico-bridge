51a4c0a176b4c4a8c744a193a86c79b895c8b07d
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2021-01-26 03:21:34 -0800

    Initial commit with project skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# scopehal-pico-bridge
+
+Socket servers for Pico Technology instruments allowing remote access via libscopehal.
---
+++ src/ps6000d/main.cpp
@@ -0,0 +1,100 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* ps6000d                                                                                                              *
+*                                                                                                                      *
+* Copyright (c) 2012-2021 Andrew D. Zonenberg                                                                          *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+/**
+	@file
+	@author Andrew D. Zonenberg
+	@brief Program entry point
+ */
+
+#ifdef _WIN32
+#include <windows.h>
+#include <shlwapi.h>
+#endif
+
+#include "../../lib/log/log.h"
+#include "../../lib/xptools/Socket.h"
+#include <thread>
+
+using namespace std;
+
+void help();
+
+void help()
+{
+	fprintf(stderr,
+			"ps6000d [general options] [logger options]\n"
+			"\n"
+			"  [general options]:\n"
+			"    --help      : this message...\n"
+			"\n"
+			"  [logger options]:\n"
+			"    levels: ERROR, WARNING, NOTICE, VERBOSE, DEBUG\n"
+			"    --quiet|-q                    : reduce logging level by one step\n"
+			"    --verbose                     : set logging level to VERBOSE\n"
+			"    --debug                       : set logging level to DEBUG\n"
+			"    --trace <classname>|          : name of class with tracing messages. (Only relevant when logging level is DEBUG.)\n"
+			"            <classname::function>\n"
+			"    --logfile|-l <filename>       : output log messages to file\n"
+			"    --logfile-lines|-L <filename> : output log messages to file, with line buffering\n"
+			"    --stdout-only                 : writes errors/warnings to stdout instead of stderr\n"
+	);
+}
+
+int main(int argc, char* argv[])
+{
+	//Global settings
+	Severity console_verbosity = Severity::NOTICE;
+
+	//Parse command-line arguments
+	for(int i=1; i<argc; i++)
+	{
+		string s(argv[i]);
+
+		//Let the logger eat its args first
+		if(ParseLoggerArguments(i, argc, argv, console_verbosity))
+			continue;
+
+		if(s == "--help")
+		{
+			help();
+			return 0;
+		}
+		else if(s[0] == '-')
+		{
+			fprintf(stderr, "Unrecognized command-line argument \"%s\", use --help\n", s.c_str());
+			return 1;
+		}
+	}
+
+	//Set up logging
+	g_log_sinks.emplace(g_log_sinks.begin(), new ColoredSTDLogSink(console_verbosity));
+
+	return 0;
+}
